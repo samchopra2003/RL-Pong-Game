6bb703155d989f14bf0f96e6a4d3a886c3a230c2
samarth <samarthchoprapws@gmail.com>
2023-10-26 22:36:19 -0400

    improved policy to approx 0 reward in 1000 episodes

--- PPOTrainer.py
+++ PPOTrainer.py
@@ -11,12 +11,13 @@ MAX_POLICY_TRAIN_ITERS = 5
 VALUE_TRAIN_ITERS = 5
 POLICY_LR = 2.5e-4
 VALUE_LR = 1e-3
+BETA = 0.01
 
 
 class PPOTrainer:
     def __init__(self,
                  actor_critic,
-                 ppo_clip_val=PPO_CLIP_VAL,
+                 # ppo_clip_val=PPO_CLIP_VAL,
                  target_kl_div=TARGET_KL_DIV,
                  max_policy_train_iters=MAX_POLICY_TRAIN_ITERS,
                  value_train_iters=VALUE_TRAIN_ITERS,
@@ -24,7 +25,7 @@ class PPOTrainer:
                  value_lr=VALUE_LR
                  ):
         self.ac = actor_critic
-        self.ppo_clip_val = ppo_clip_val
+        # self.ppo_clip_val = ppo_clip_val
         self.target_kl_div = target_kl_div
         self.max_policy_train_iters = max_policy_train_iters
         self.value_train_iters = value_train_iters
@@ -37,41 +38,25 @@ class PPOTrainer:
                        list(self.ac.policy_layers.parameters())
         self.value_optim = optim.Adam(value_params, lr=value_lr)
 
-    def train_policy(self, obs, actions, old_log_probs, gaes, beta=0.01):
+    def train_policy(self, obs, actions, old_log_probs, gaes, beta=BETA, epsilon=PPO_CLIP_VAL):
         """
         :param new_logits: new_logits mapped to observations
         :param actions: actions
         :param old_log_probs: old log probabilities
         :param gaes: General Advantgae Estimations
         :param beta: Coefficient for entropy (exploration)
+        :param epsilon: Clipping Parameter
         :return:
         """
         for _ in range(self.max_policy_train_iters):
             self.policy_optim.zero_grad()  # reset gradient
 
-            # new_logits = []
-            # for i in range(len(obs)):
-            #     cur_obs = torch.tensor([obs[i]], dtype=torch.float32, device=DEVICE)
-            #     new_logits.append(self.ac.policy(obs))
-
-            # new_logits = self.ac.policy(torch.tensor([obs], dtype=torch.float32, device=DEVICE))
-
-            # new_logits = self.ac.policy(obs[0].unsqueeze(0))
             new_logits = self.ac.policy(obs)
             new_logits = Categorical(logits=new_logits)
             new_log_probs = new_logits.log_prob(actions)
 
-            # print(new_logits)
-
-            # new_log_probs = []
-            # for i in range(len(new_logits)):
-            #     cur_logits = Categorical(logits=new_logits[i])
-            #     new_log_probs.append(cur_logits.log_prob(actions[i]))
-
-            # new_log_probs = torch.stack(new_log_probs)
-
             policy_ratio = torch.exp(new_log_probs - old_log_probs)
-            clipped_ratio = policy_ratio.clamp(1 - self.ppo_clip_val, 1 + self.ppo_clip_val)
+            clipped_ratio = policy_ratio.clamp(1 - epsilon, 1 + epsilon)
 
             clipped_loss = clipped_ratio * gaes
             full_loss = policy_ratio * gaes
--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 import gymnasium as gym
 
 import torch
-from torch import optim
 from torch.distributions.categorical import Categorical
 import numpy as np
-import cv2
-import matplotlib.pyplot as plt
 
 from ActorCriticNetwork import ActorCriticNetwork
 from PPOTrainer import PPOTrainer
-from calc_util import discount_rewards, calculate_gaes
-from util import preprocess_img, obs_to_nn_shape
+from helper_utils.calc_rewards import discount_rewards, calculate_gaes
+from helper_utils.preprocess_imgs import preprocess_img
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 ENVIRONMENT = "ALE/Pong-v5"
 # ENVIRONMENT = "PongDeterministic-v4"
 # ENVIRONMENT = "ALE/Pong-ram-v5"
-EPISODES = 200
+EPISODES = 1000
 LOGGING_FREQ = 5
 
 NOOP = 0
@@ -37,19 +34,12 @@ def rollout(model, env, max_steps=1000, n_rand=5):
     obs = env.reset()
     # print(env.action_space)
     # print(env.observation_space)
-    # gray_frame = cv2.cvtColor(obs[0], cv2.COLOR_BGR2GRAY)
-    # print(gray_frame.shape)
     episode_reward = 0
 
     for i in range(max_steps):
         preprocessed_img = []
         val = 0
         action_log_prob = []
-        # if i == 0:
-        #     print(obs[0].shape)
-        # else:
-        #     print(obs.shape)
-        # print("len of obs = ", len(obs))
 
         # take nrand actions at the start
         if i < n_rand:
@@ -62,14 +52,9 @@ def rollout(model, env, max_steps=1000, n_rand=5):
             # plt.show()
 
             logits, val = model(torch.tensor([preprocessed_img], dtype=torch.float32, device=DEVICE))
-            # logits, val = model(torch.tensor([obs[0]], dtype=torch.float32, device=DEVICE))
-            # print(logits, val)
             action_distribution = Categorical(logits=logits)
-            # print(action_distribution)
             action = action_distribution.sample()
-            # print(action)
             action_log_prob = action_distribution.log_prob(action).item()
-            # action_log_prob = action_distribution.probs[0, action].detach().cpu().numpy()
             action, val = action.item(), val.item()
 
             og_action = action
@@ -89,7 +74,6 @@ def rollout(model, env, max_steps=1000, n_rand=5):
         action = og_action
 
         if terminated or truncated:
-            # print("lol")
             break
 
         obs = next_obs
@@ -101,10 +85,7 @@ def rollout(model, env, max_steps=1000, n_rand=5):
             # gray_frame = cv2.cvtColor(obs, cv2.COLOR_BGR2GRAY)
             obs_data.append(preprocessed_img)
 
-    # print(len(train_data[0]), len(train_data[1]), len(train_data[2]), len(train_data[3]), len(train_data[4]))
     train_data = [np.asarray(x) for x in train_data]
-    # max_shape = max([len(arr) for arr in train_data])
-    # train_data = [np.reshape(arr, max_shape) for arr in train_data]
 
     # obtain advantage estimates using rewards and values
     train_data[3] = calculate_gaes(train_data[2], train_data[3])
@@ -118,12 +99,11 @@ if __name__ == '__main__':
     # print(env.observation_space.shape, env.action_space.n)
     model = ActorCriticNetwork(env.observation_space.shape[0], env.action_space.n)
     model = model.to(DEVICE)
-    # train_data, reward = rollout(model, env)
-    # print(train_data, reward)
     ppo_trainer = PPOTrainer(model)
 
     # Hyperparameters
     beta = 0.01
+    epsilon = 0.2
 
     # Training Loop
     episode_rewards = []
@@ -135,40 +115,6 @@ if __name__ == '__main__':
 
         # POLICY data
         obs = torch.tensor(train_data[0][permute_idxs], dtype=torch.float32, device=DEVICE)
-        # obs = train_data[0][permute_idxs]
-
-        # GOLDEN DATA
-        # obs = torch.tensor([obs_data[0]], dtype=torch.float32, device=DEVICE)
-        # print('yolo')
-        # poly = model.policy(obs)
-        # print('done yolo')
-
-        # permuted_idxs_obs_data = []
-        # for p_idx in permute_idxs:
-        #     permuted_idxs_obs_data.append(obs_data[p_idx])
-        #
-
-        # map obs to new policy logits
-        # new_logits = []
-        # for i in permute_idxs:
-        #     cur_obs = torch.tensor([obs_data[i]], dtype=torch.float32, device=DEVICE)
-        #     new_logits.append(model.policy(cur_obs))
-
-        # new_logits = torch.stack(new_logits)
-
-        # map obs to values
-        # new_vals = []
-        # for i in permute_idxs:
-        #     cur_obs = torch.tensor([obs_data[i]], dtype=torch.float32, device=DEVICE)
-        #     new_vals.append(model.value(cur_obs))
-
-        # new_vals = torch.stack(new_vals)
-
-        # obs = [torch.from_numpy(obs_array).float().unsqueeze(0).to('cuda:0') for obs_array in obs]
-        # obs = torch.from_numpy(obs).float().unsqueeze(0).to('cuda:0')
-        # print(len(obs))
-        # obs = obs_to_nn_shape(train_data[0])
-        # print(obs.shape)
         actions = torch.tensor(train_data[1][permute_idxs], dtype=torch.int32, device=DEVICE)
         gaes = torch.tensor(train_data[3][permute_idxs], dtype=torch.float32, device=DEVICE)
         action_log_probs = torch.tensor(train_data[4][permute_idxs], dtype=torch.float32, device=DEVICE)
@@ -177,13 +123,13 @@ if __name__ == '__main__':
         returns = torch.tensor(discount_rewards(train_data[2][permute_idxs]), dtype=torch.float32, device=DEVICE)
 
         # POLICY + VALUE training (train model)
-        # ppo_trainer.train_policy(new_logits, actions, action_log_probs, gaes)
-        # ppo_trainer.train_value(new_vals, returns)
-        ppo_trainer.train_policy(obs.unsqueeze(1), actions, action_log_probs, gaes, beta=beta)
+        ppo_trainer.train_policy(obs.unsqueeze(1), actions, action_log_probs, gaes, beta=beta, epsilon=epsilon)
         ppo_trainer.train_value(obs.unsqueeze(1), returns)
 
         # this reduces exploration in later runs
-        beta *= 0.995
+        beta *= .995
+        # the clipping parameter reduces as time goes on
+        # epsilon *= .999
 
         if (episode_idx + 1) % LOGGING_FREQ == 0:
             print('Episode {} | Avg. Rewards {:.1f}'.format(
@@ -192,13 +138,3 @@ if __name__ == '__main__':
 
     # save policy
     torch.save(model, 'PPO_pong.pkl')
-
-#     observation, info = env.reset()
-#     for _ in range(1000):
-#         action = env.action_space.sample()  # agent policy that uses the observation and info
-#         observation, reward, terminated, truncated, info = env.step(action)
-#
-#         if terminated or truncated:
-#             observation, info = env.reset()
-#
-#     env.close()
